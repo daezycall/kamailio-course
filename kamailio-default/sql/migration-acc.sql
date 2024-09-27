51874b489ff5d81fb3dab013e1bf106747e88a2d
ondrej.raso <ondrej.raso@mbae-it.com>
2024-09-27 14:50:24 +0200

    commit, added some custom changes to both tables
    Signed-off-by: ondrej.raso <ondrej.raso@mbae-it.com>

--- kamailio-default/sql/migration-acc.sql
+++ kamailio-default/sql/migration-acc.sql
@@ -4,7 +4,20 @@ ADD COLUMN (
     src_ip VARCHAR(255),
     src_domain VARCHAR(255),
     dst_user VARCHAR(255),
-    call_id VARCHAR(255)
+    call_id VARCHAR(255),
+    invtime VARCHAR(255),
+    dst_ip VARCHAR(255)    
+);
+
+
+
+
+
+
+
+ALTER table acc
+ADD COLUMN (
+    dst_ip VARCHAR(255)
 );
 
 
